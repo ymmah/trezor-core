4916393139ba6557b68b5385d6c7b2ca1244c1c0
slush <slush@satoshilabs.com>
2017-12-12 19:19:55 +0100

    Add support for protobuf Sint32 type

--- src/protobuf.py
+++ src/protobuf.py
@@ -54,6 +54,8 @@ async def dump_uvarint(writer, n):
 class UVarintType:
     WIRE_TYPE = 0
 
+class Sint32Type:
+    WIRE_TYPE = 0
 
 class BoolType:
     WIRE_TYPE = 0
@@ -145,6 +147,8 @@ async def load_message(reader, msg_type):
 
         if ftype is UVarintType:
             fvalue = ivalue
+        elif ftype is Sint32Type:
+            fvalue = (ivalue >> 1) ^ ((ivalue << 31) & 0xffffffff)
         elif ftype is BoolType:
             fvalue = bool(ivalue)
         elif ftype is BytesType:
@@ -200,6 +204,9 @@ async def dump_message(writer, msg):
 
             if ftype is UVarintType:
                 await dump_uvarint(writer, svalue)
+
+            elif ftype is Sint32Type:
+                await dump_uvarint(writer, ((svalue << 1) & 0xffffffff) ^ (svalue >> 31))
 
             elif ftype is BoolType:
                 await dump_uvarint(writer, int(svalue))
